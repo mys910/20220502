1cbd51f82158f14a76073f005d2f1ba051ba1af6
SungYongMo <mys910@gmail.com>
2022-05-02 09:13:36 +0900

    1234

--- main.cpp
+++ main.cpp
@@ -3,8 +3,20 @@ using namespace std;
 
 int main()
 {
-	int Time;
-	Time = 3;
+	int A;
+	A = 10;
 
-	cout << Time << endl;
+	cout << A + 1 << endl;
+	cout << A - 1 << endl;
+	cout << A * 2 << endl;
+	cout << A / 2 << endl;
+
+	float B;
+	B = 1.9f;
+
+	cout << B + 0.1 << endl;
+	cout << B - 0.9 << endl;
+	cout << B * 10 << endl;
+	cout << B / 1.9 << endl;
+
 }
